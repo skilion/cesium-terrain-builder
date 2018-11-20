6bceff69dbd7d7cb0ecb6d47fd3a2592bb612696
skilion <andrea.9940@gmail.com>
2018-11-20 23:06:14 +0100

    Change CTBZOutputStream to work in memory

--- src/CTBZOutputStream.cpp
+++ src/CTBZOutputStream.cpp
@@ -19,17 +19,108 @@
  * @brief This defines the `CTBZOutputStream` and `CTBZFileOutputStream` classes
  */
 
+#include <assert.h>
+
 #include "CTBException.hpp"
 #include "CTBZOutputStream.hpp"
 
 using namespace ctb;
+
+ctb::CTBZOutputStream::CTBZOutputStream() {
+  stream.zalloc = Z_NULL;
+  stream.zfree = Z_NULL;
+  stream.opaque = Z_NULL;
+  init();
+}
+
+ctb::CTBZOutputStream::~CTBZOutputStream() {
+  end();
+}
+
+/**
+ * @details 
+ * Compresses a sequence of memory pointed by ptr.
+ */
+uint32_t
+ctb::CTBZOutputStream::write(const void *ptr, uint32_t size) {
+  deflateRound(ptr, size, Z_NO_FLUSH);
+}
+
+/**
+ * @details 
+ * Initializes the stream.
+ */
+void
+ctb::CTBZOutputStream::init() {
+  int ret = deflateInit2(
+    &stream,
+    Z_DEFAULT_COMPRESSION,
+    Z_DEFLATED,
+    16 + MAX_WBITS,
+    8,
+    Z_DEFAULT_STRATEGY
+  );
+  if (ret != Z_OK) {
+    throw CTBException("Could not initialize zlib");
+  }
+}
+
+/**
+ * @details 
+ * Cleans the memory allocated by zlib.
+ */
+void
+ctb::CTBZOutputStream::end() {
+  deflateEnd(&stream);
+}
+
+/**
+ * @details 
+ * Finalizes the stream.
+ */
+void
+ctb::CTBZOutputStream::finish() {
+  deflateRound(Z_NULL, 0, Z_FINISH);
+}
+
+/**
+ * @details 
+ * Resets the stream, making it ready to accept new data.
+ */
+void
+ctb::CTBZOutputStream::reset() {
+  buffer.clear();
+  deflateReset(&stream);
+}
+
+/**
+ * @details 
+ * Performs a round of deflate().
+ */
+void
+ctb::CTBZOutputStream::deflateRound(const void *data, size_t size, int flush) {
+  const size_t TMPSIZE = 4 * 1024;
+  uint8_t temp[TMPSIZE];
+  stream.next_in = (Bytef*) data;
+  stream.avail_in = size;
+  do {
+    stream.next_out = temp;
+    stream.avail_out = TMPSIZE;
+    if (deflate(&stream, flush) == Z_STREAM_ERROR) {
+      throw CTBException("Compression failed");
+    }
+    size_t have = TMPSIZE - stream.avail_out;
+    buffer.insert(buffer.end(), temp, temp + have);
+  } while (stream.avail_out == 0);
+  assert(stream.avail_in == 0);
+}
 
 /**
  * @details 
  * Writes a sequence of memory pointed by ptr into the GZFILE*.
  */
 uint32_t
-ctb::CTBZOutputStream::write(const void *ptr, uint32_t size) {
+ctb::CTBZFileOutputStream::write(const void *ptr, uint32_t size) {
   if (size == 1) {
     int c = *((const char *)ptr);
     return gzputc(fp, c) == -1 ? 0 : 1;
@@ -39,7 +130,7 @@ ctb::CTBZOutputStream::write(const void *ptr, uint32_t size) {
   }
 }
 
-ctb::CTBZFileOutputStream::CTBZFileOutputStream(const char *fileName) : CTBZOutputStream(NULL) {
+ctb::CTBZFileOutputStream::CTBZFileOutputStream(const char *fileName) {
   gzFile file = gzopen(fileName, "wb");
 
   if (file == NULL) {
--- src/CTBZOutputStream.hpp
+++ src/CTBZOutputStream.hpp
@@ -22,6 +22,8 @@
  * @brief This declares and defines the `CTBZOutputStream` class
  */
 
+#include <vector>
+
 #include "zlib.h"
 #include "CTBOutputStream.hpp"
 
@@ -30,26 +32,50 @@ namespace ctb {
   class CTBZOutputStream;
 }
 
-/// Implements CTBOutputStream for `GZFILE` object
+/// Implements CTBOutputStream for compressing a stream in memory
 class CTB_DLL ctb::CTBZOutputStream : public ctb::CTBOutputStream {
 public:
-  CTBZOutputStream(gzFile gzptr): fp(gzptr) {}
+  CTBZOutputStream();
+  ~CTBZOutputStream();
 
   /// Writes a sequence of memory pointed by ptr into the stream
   virtual uint32_t write(const void *ptr, uint32_t size);
 
+  void finish();
+  void reset();
+
+  // Use after calling finish
+  const uint8_t *data() { return buffer.data(); };
+  size_t size() { return buffer.size(); };
+
 protected:
-  /// The underlying GZFILE*
-  gzFile fp;
+  /// Internal stream state
+  z_stream stream;
+  /// Buffer for the compressed data
+  std::vector<uint8_t> buffer;
+
+  /// Initializes the zlib stream
+  void init();
+  /// Cleans the memory allocated by zlib
+  void end();
+  /// Performs a round of deflate()
+  void deflateRound(const void *ptr, size_t size, int flush);
 };
 
 /// Implements CTBOutputStream for gzipped files
-class CTB_DLL ctb::CTBZFileOutputStream : public ctb::CTBZOutputStream {
+class CTB_DLL ctb::CTBZFileOutputStream : public ctb::CTBOutputStream {
 public:
   CTBZFileOutputStream(const char *fileName);
  ~CTBZFileOutputStream();
 
+  /// Writes a sequence of memory pointed by ptr into the stream
+  virtual uint32_t write(const void *ptr, uint32_t size);
+
   void close();
+
+protected:
+  /// The underlying GZFILE*
+  gzFile fp;
 };
 
 #endif /* CTBZOUTPUTSTREAM_HPP */
